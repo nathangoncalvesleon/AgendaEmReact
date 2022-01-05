90cc59960313901ea42f0e76a8db76ae90b8e528
nathangoncalvesleon <nathangworks@gmail.com>
2022-01-05 17:53:22 -0300

    Lista em React

---
+++ todo/src/App.tsx
@@ -0,0 +1,45 @@
+import * as C from "./App.styles"
+import { useState } from "react";
+import { Item } from "./types/Item";
+import { ListItem } from "./components/ListItem";
+import { AddArea } from "./components/AddArea";
+
+const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    {
+      id:1, name:'test', done:false
+    },
+    {
+      id:2, name:'test2', done:false
+    }
+  ])  
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length +1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+  return (
+  <>
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        <AddArea onEnter={handleAddTask} />
+        {list.map((item, index)=>
+        (
+         <ListItem item={item} key={index}/>
+        
+        ))}
+      </C.Area>
+    </C.Container>
+  </>
+    
+  );
+}
+
+export default App;
